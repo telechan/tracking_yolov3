edaaa6a9e252d14986c335b4eafe818c1b27d5af
telechan <sou.s0804@gmail.com>
2019-11-19 23:14:16 +0900

    modify yolo_video.py

--- yolo_video.py
+++ yolo_video.py
@@ -5,19 +5,6 @@ from PIL import Image
 import numpy as np
 import cv2
 
-# def detect_img(yolo):
-#     while True:
-#         img = input('Input image filename:')
-#         try:
-#             image = Image.open(img)
-#         except:
-#             print('Open Error! Try again!')
-#             continue
-#         else:
-#             r_image = yolo.detect_image(image)
-#             r_image.show()
-#     yolo.close_session()
-
 def detect_img(yolo):
     while True:
         img = input('Input image filename:')
